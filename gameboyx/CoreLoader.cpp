c22126ae998f3cddd60d64c80e764d2e56ff55d5
Matthias Merkl <matthias-merkl@gmx.de>
2024-06-26 14:30:31 +0200

    update for backend. implemented basic approach for dynamically loading emulated hardware components (DLLs) during runtime -> move different hardware to seperate projects and compile to dynamic library

---
+++ gameboyx/CoreLoader.cpp
@@ -0,0 +1,95 @@
+#include "CoreLoader.h"
+
+#include "HardwareMgr.h"
+#include "helper_functions.h"
+#include "logger.h"
+
+#include <windows.h>
+
+
+namespace Emulation {
+    std::vector<std::pair<console_ids, functions>> CoreLoader::loadedCores = {};
+
+    inline const std::string DLL_EXT = "dll";
+
+    inline const std::vector<std::string> FUNCTION_NAMES = {
+        "GetConsoleIDs"
+    };
+
+    std::string get_function_name(std::string _in){
+        auto it = std::find_if(FUNCTION_NAMES.begin(), FUNCTION_NAMES.end(), [&_in](const std::string& _e) { return _e.compare(_in) == 0; });
+        if (it != FUNCTION_NAMES.end()) {
+            return *it;
+        } else {
+            LOG_ERROR("[DLL Loader] Function not found in core: ", _in);
+            return {};
+        }
+    }
+
+    bool check_function_pointer(const func_ptr _fn_ptr, const std::string& _fn_name) {
+        try {
+            if (!_fn_ptr) { throw _fn_name; }
+            return true;
+        } catch (std::string fn_name) {
+            LOG_ERROR("[DLL Loader] Could not acquire pointer to function: ", fn_name);
+            return false;
+        }
+    }
+
+    void CoreLoader::LoadCores(std::string _dll_folder) {
+        std::vector<std::string> dlls;
+        Backend::FileIO::get_files_in_path(dlls, _dll_folder);
+
+        for (auto& path : dlls) {
+            if (Helpers::split_string(path, ".").back().compare(DLL_EXT) != 0) {
+                continue;
+            }
+
+            const size_t c_size = strlen(path.c_str()) + 1;
+            wchar_t* lpcw_str = new wchar_t[c_size];
+            mbstowcs(lpcw_str, path.c_str(), c_size);
+
+            HINSTANCE hGetProcIDDLL = LoadLibrary(lpcw_str);
+            if (!hGetProcIDDLL) {
+                LOG_ERROR("[DLL Loader] Could not load core: ", lpcw_str);
+                continue;
+            }
+
+            GetConsoleIDs_ptr fn_console_id = (GetConsoleIDs_ptr)GetProcAddress(hGetProcIDDLL, get_function_name("GetConsoleIDs").c_str());
+            if (!check_function_pointer((func_ptr)fn_console_id, "GetConsoleIDs")) {
+                continue;
+            }
+
+            try {
+                if (!fn_console_id) { throw "GetConsoleIDs"; }
+            } catch (std::string fn_name) {
+                LOG_ERROR("[DLL Loader] Could not acquire pointer to function: ", fn_name);
+                continue;
+            }
+            
+            auto ids = fn_console_id();
+            for (const auto& id : ids) {
+                if (std::find_if(loadedCores.begin(), loadedCores.end(), [&id](auto& _e) { return _e.first == id; }) == loadedCores.end()) {
+
+                    loadedCores.emplace_back();
+                    auto& core_data = loadedCores.back();
+                    core_data.first = id;
+                    auto& core_fn = core_data.second;
+                    for (size_t i = 0; i < FUNCTION_NAMES.size(); i++) {
+                        core_fn.ptrs[i] = (func_ptr)GetProcAddress(hGetProcIDDLL, FUNCTION_NAMES[i].c_str());
+
+                        if (!check_function_pointer(core_fn.ptrs[i], FUNCTION_NAMES[i])) {
+                            // TODO: further error handling
+                            continue;
+                        }
+                    }
+
+                } else if(std::find_if(FILE_EXTS.begin(), FILE_EXTS.end(), [&id](auto& _e) { _e.first == id; }) != FILE_EXTS.end()) {
+                    LOG_WARN("[DLL Loader] Core has already been loaded: ", FILE_EXTS.at(id));
+                } else {
+                    LOG_WARN("[DLL Loader] Could not find requested core: ", id);
+                }
+            }
+        }
+    }
+}
---
+++ gameboyx/CoreLoader.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "VHardwareTypes.h"
+
+#include <string.h>
+#include <vector>
+
+namespace Emulation {
+    inline const size_t FUNC_NUM = 1;
+
+    // generic function pointer type
+    typedef void(__stdcall* func_ptr)();
+
+    typedef std::vector<console_ids>(__stdcall* GetConsoleIDs_ptr)();
+
+    typedef struct function_ptrs {
+        std::vector<console_ids>(__stdcall* GetConsoleIDs_ptr)();
+    };
+
+    typedef union functions {
+        function_ptrs by_type;
+        func_ptr ptrs[FUNC_NUM];
+    };
+
+    class CoreLoader {
+    public:
+        static void LoadCores(std::string _dll_path);
+
+    private:
+        CoreLoader() = delete;
+        ~CoreLoader() = delete;
+
+        static std::vector<std::pair<console_ids, functions>> loadedCores;
+    };
+}
